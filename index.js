ba50cd141cf268a1059c7f8db2aa080d504c0da5
Parsha Pourkhomami <parshap+git@gmail.com>
2013-08-19 02:31:40 -0700

    Write metadata and rename files WIP

--- index.js
+++ index.js
@@ -7,25 +7,128 @@ var FILES = [
 	"test-album/03 Letting You.mp3",
 ];
 
-var es = require("event-stream"),
-	unique = require("unique-stream"),
-	async = require("async"),
-	taglib = require("taglib"),
-	acoustid = require("acoustid");
+var es = require("event-stream");
 
 var trackStream = createTrackStream(),
 	releaseRepo = createReleaseRepository();
 
 es.readArray(FILES)
 	.pipe(trackStream)
-	.pipe(releaseRepo)
-	.pipe(es.through(null, function() {
-		releaseRepo.createReadStream()
-			.pipe(es.through(logRelease));
-	}));
+	.pipe(releaseRepo);
 
-function logRelease(release) {
-	var str = getArtistsString(release.artists) +
+releaseRepo.on("end", function() {
+	var releases = releaseRepo.toArray();
+	// @TODO Reduce to groups of overlapping releases
+	selectRelease(releases, function(err, release) {
+		updateRelease(release, function(err) {
+			console.log("done", err);
+		});
+	});
+});
+
+function selectRelease(releases, callback) {
+	releases = releases.sort(function(a, b) {
+		return a.tracks.length - b.tracks.length;
+	}).reverse();
+	console.log(releases.length, "Releases Found:");
+	recursivePromptList("Select a release: ", releases, callback);
+}
+
+function recursivePromptList(text, releases, callback) {
+	promptList(text, releases, function(err, value) {
+		if (err) return recursivePromptList(text, releases, callback);
+		callback(null, value);
+	});
+}
+
+// ES6 isInteger Polyfill https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger#Polyfill
+function isInteger(val) {
+	return typeof val === "number" &&
+		isFinite(val) &&
+		val > -9007199254740992 &&
+		val < 9007199254740992 &&
+		Math.floor(val) === val;
+}
+
+var read = require("read");
+function prompt(text, callback) {
+	read({ prompt: text}, callback);
+}
+
+function promptList(text, list, callback) {
+	consoleList(list);
+	prompt(text, function(err, val) {
+		var index = Number(val);
+		if (isInteger(index) && index > 0 && index <= list.length) {
+			callback(null, list[index - 1]);
+		}
+		else {
+			callback(new Error("Invalid selection"));
+		}
+	});
+}
+
+function consoleList(list) {
+	list.forEach(function(item, i) {
+		console.log("", (i+1) + ". ", item.toString());
+	});
+}
+
+// Write metadata and rename files
+function updateRelease(release, callback) {
+	async.series([
+		writeTags.bind(null, release),
+		rename.bind(null, release),
+	], callback);
+}
+
+function writeTags(release, callback) {
+	var albumArtist = getArtistString(release.artists);
+	async.forEach(release.tracks, function(track, callback) {
+		var artist = getArtistString(track.artists),
+			isSameArtist = artist === albumArtist;
+		console.log("write", track.path);
+		ffmetadata.write(track.path, {
+			album: release.title,
+			artist: artist,
+			album_artist: isSameArtist ? null : albumArtist,
+			track: track.position + "/" + track.discTrackCount,
+			disc: track.discPosition + "/" + release.discCount,
+			title: track.title,
+		}, callback);
+	}, callback);
+}
+
+function rename(release, callback) {
+	async.forEach(release.tracks, function(track, callback) {
+		rebasename(track.path, getFileName(track), function(err, newPath) {
+			if (err) return callback(err);
+			track.path = newPath;
+		});
+	}, callback);
+}
+
+var path = require("path"),
+	fs = require("fs");
+// @TODO Extract
+function rebasename(p, basename, callback) {
+	var ext = path.extname(p),
+		newName = basename + ext,
+		dirname = path.dirname(p),
+		newPath = path.join(dirname, newName);
+	return fs.rename(p, newPath, function(err) {
+		callback(err, newPath);
+	});
+}
+
+var pad = require("pad");
+function getFileName(track) {
+	// {position}. {title}
+	return pad(2, String(track.position), "0") + ". " + track.title;
+}
+
+function getReleaseString(release) {
+	var str = getArtistString(release.artists) +
 		" / " +
 		release.title;
 	if (release.country) {
@@ -33,7 +136,13 @@ function logRelease(release) {
 	}
 	str += " (" + release.tracks.length + " of " +
 		release.trackCount + " tracks)";
-	console.log(str);
+	return str;
+}
+
+function getArtistString(artists) {
+	return artists.map(function(artist) {
+		return artist.name;
+	}).join("; ");
 }
 
 function createReleaseRepository() {
@@ -104,12 +213,15 @@ function createReleaseRepository() {
 			position: track.position,
 			discTrackCount: medium.track_count,
 			discPosition: medium.position,
+			recording: recording,
+			path: file.file,
+			tags: file.tags,
 			// @TODO Recording & File path
 		});
 	}
 
 	function createRelease(release, group, recording, file) {
-		return {
+		var rel = {
 			id: release.id,
 			title: group.title,
 			type: group.type,
@@ -120,16 +232,14 @@ function createReleaseRepository() {
 			country: release.country,
 			tracks: [],
 		};
+		rel.toString = function() {
+			return getReleaseString(this);
+		};
+		return rel;
 	}
 
 	repo.data = {};
 	return repo;
-}
-
-function getArtistsString(artists) {
-	return artists.map(function(artist) {
-		return artist.name;
-	}).join("; ");
 }
 
 // Map file paths to Track objects
@@ -137,16 +247,22 @@ function createTrackStream() {
 	return es.map(getTrackData);
 }
 
+var async = require("async"),
+	ffmetadata = require("ffmetadata"),
+	acoustid = require("acoustid");
+
 // Get track data from track file path
 function getTrackData(file, callback) {
 	async.parallel({
-		"tags": taglib.read.bind(null, file),
+		"tags": ffmetadata.read.bind(null, file),
 		"acoustid": getAcoustID.bind(null, file),
 	}, function(err, results) {
+		var first = results.acoustid[0],
+			recordings = first ? first.recordings : [];
 		callback(err, {
 			file: file,
-			tags: results.tags[0],
-			recordings: results.acoustid[0].recordings,
+			tags: results.tags,
+			recordings: recordings,
 		});
 	});
 }
