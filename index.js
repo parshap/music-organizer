e5d1660fc8d7662eb68e7ce912263435aee311fb
Parsha Pourkhomami <parshap+git@gmail.com>
2013-09-23 12:29:35 -0700

    Extract logging logic

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // jshint node:true
 "use strict";
 
-var PACKAGE = require("./package.json");
-
 var createFileStream = require("./lib/files"),
 	createTrackStream = require("./lib/tracks"),
 	createParseStream = require("./lib/parse"),
@@ -26,13 +24,6 @@ module.exports = function(path) {
 		dirRenamer = createDirRenamer(),
 		buffer = createBufferStream();
 
-	logger({
-		files: fileStream,
-		tracks: trackStream,
-		tags: tagPicker,
-		writer: writer,
-	});
-
 	fileStream
 		.pipe(trackStream)
 		.pipe(parser)
@@ -50,44 +41,12 @@ module.exports = function(path) {
 	fileRenamer.on("end", function() {
 		buffer.replay(dirRenamer);
 	});
+
+	// Return some of the streams for logging and introspection
+	return {
+		files: fileStream,
+		tracks: trackStream,
+		tags: tagPicker,
+		writer: writer,
+	};
 };
-
-// -- Logging
-
-var progress = require("./lib/stream-progress");
-
-function logger(streams) {
-	logIntro();
-	logReading(streams);
-	logWriting(streams);
-}
-
-function logIntro() {
-	console.log("album-organizer", "v" + PACKAGE.version);
-	console.log();
-}
-
-function logReading(streams) {
-	var prog = progress(streams.tracks);
-	streams.files.pipe(count(function(length) {
-		console.log("Reading", length, "files");
-		prog.render();
-	}));
-}
-
-function logWriting(streams) {
-	var prog = progress(streams.writer);
-	streams.tags.pipe(count(function(length) {
-		console.log("Writing", length, "files");
-		prog.render();
-	}));
-}
-
-var reduce = require("stream-reduce");
-function count(fn) {
-	var stream = reduce(function(acc) {
-		return acc + 1;
-	}, 0);
-	stream.on("data", fn);
-	return stream;
-}
